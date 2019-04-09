b6b2ec9d5dc460a49cf3ebf782581914e261e606
TF997 <48859192+TF997@users.noreply.github.com>
2019-04-09 22:58:54 +0100

    Delete Gen.h

--- Gen.h
+++
@@ -1,8 +0,0 @@
-#ifndef cG_H
-#define cG_H
-#include <vector>
-#include <string>
-#include "OptionsC.h"
-using namespace std;
-int Gen(vector<string> NameSelection, vector<string> RaceSelection, Options& MyOptions,int CharNum);
-#endif
